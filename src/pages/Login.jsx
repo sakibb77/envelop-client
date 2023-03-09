e3633abc8081c6bf12f60b6c2a3fdae1ede74349
sakib biswas <sakibbiswas418@gmail.com>
2023-03-09 08:36:39 -0800

    login form added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/Button";
+import FormControl from "../components/FormControl";
+import SectionTitle from "../components/SectionTitle";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [fromFields, setFromFields] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    console.log(fromFields);
+
+    //clear fields
+    setFromFields({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="login flex flex-col justify-center items-center mt-14">
+      <form onSubmit={handleLogin} className="flex flex-col gap-5" action="">
+        <SectionTitle title={"Login"} />
+
+        {/* email input */}
+        <FormControl
+          label="email"
+          labelInner="Email"
+          inputType="email"
+          placeholder="write your email"
+          fromFields={fromFields}
+          setFromFields={setFromFields}
+        />
+
+        {/* password input */}
+        <FormControl
+          label="password"
+          labelInner="Password"
+          inputType="password"
+          placeholder="write your password"
+          fromFields={fromFields}
+          setFromFields={setFromFields}
+        />
+
+        <Button text={"register"} submit />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
